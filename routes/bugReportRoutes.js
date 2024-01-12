c63e2caa704839e7168472ad79955bc6a59934a6
Marcus <wendpanga1004@gmail.com>
2024-01-12 22:43:49 +0100

    all

--- routes/bugReportRoutes.js
+++ routes/bugReportRoutes.js
@@ -1,19 +1,14 @@
 const express = require('express');
 const bugReportController = require('../controllers/bugReportController');
 const commentsRouter = require('./commentsRoutes');
-// const reviewRouter = require('./../routes/reviewsRoutes');
 const imageRouter = require('./imagesRoutes');
 const authenticatioController = require('../controllers/authenticatioController');
-// const bugFixesController = require('../controllers/bugFixesController');
 const bugFixRouter = require('./bugFixesRoutes');
 
 const router = express.Router({ mergeParams: true });
 
 router.use('/:bug_id/comments', commentsRouter);
-// router.use('/:bug_id/bug_fixes/:bug_fixes_id/comments', commentsRouter);
-// router.use('/:bug_id/bug_fixes/:bug_fixes_id/reviews', reviewRouter);
 router.use('/:bug_id/image', imageRouter);
-// router.use('/:bug_report_id/bug_fixes/:bug_fixes_id/image', imageRouter);
 router.use('/:bug_id/bug_fix', bugFixRouter);
 
 router.use(authenticatioController.protect);
